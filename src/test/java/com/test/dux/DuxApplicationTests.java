2bbe6f61aeb531243fdb0bdc10760c09dd8c7179
Carlos Mamani <carlosfernando21334@gmail.com>
2025-01-20 12:14:26 -0300

    Proyecto Inicial

---
+++ src/test/java/com/test/dux/DuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.dux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
