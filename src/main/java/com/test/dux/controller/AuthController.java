f9557686d87988de6fec294f5da66efab44f1026
Carlos Mamani <carlosfernando21334@gmail.com>
2025-01-20 13:17:52 -0300

    endpoint para login

---
+++ src/main/java/com/test/dux/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.test.dux.controller;
+
+import com.test.dux.dto.CredentialDTO;
+import com.test.dux.exception.NotAuthenticatedUserException;
+import com.test.dux.service.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+    private final Logger log = LoggerFactory.getLogger(AuthController.class);
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody CredentialDTO credentialDTO) throws NotAuthenticatedUserException {
+        log.info("Se recibe peticion para iniciar sesion: {} ",credentialDTO);
+
+        Map<String, String> response = new HashMap<>();
+        String token = authService.login(credentialDTO);
+        response.put("token", token);
+
+        log.info("Se genera token jwt: {} ",token);
+        return ResponseEntity.ok(response);
+
+    }
+}
